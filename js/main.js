0df5ce93e5460aab56322b1a770a33590b68adbc
EgorVasilev <egor.vasilev.me@gmail.com>
2021-12-13 20:31:25 +0400

    implement "prefers-reduced-motion" media-query to make animations safer for people that are sensitive for it
    make the diagram a bit more responsive with "viewbox" attribute instead of strict "width/height"

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* check #plot aspect-ratio in CSS as well if you want to change it */
 const plotWidth = 900;
 const plotHeight = 500;
 const plotPadding = 40;
@@ -10,9 +11,8 @@ function fetchGDP() {
 }
 
 function setPlotSize() {
-    d3.select('#plot')
-        .attr('width', plotWidth)
-        .attr('height', plotHeight);
+    d3.select('#plot').attr('viewBox', `0 0 ${plotWidth} ${plotHeight}`);
+
 }
 
 function handleMouseOver(event, data) {
@@ -20,15 +20,15 @@ function handleMouseOver(event, data) {
 
     d3.select('.tooltip')
         .html(`${data[0]}<br>$${data[1]} Billion`)
-        .style('top', `${event.screenY + offset}px`)
-        .style('left', `${event.screenX + offset}px`)
+        .style('top', `${event.clientY + offset}px`)
+        .style('left', `${event.clientX + offset}px`)
         .attr('data-date', data[0])
-        .classed('hidden', false)
+        .classed('hidden', false);
 }
 
 function handleMouseOut() {
     d3.select('.tooltip')
-        .classed('hidden', true)
+        .classed('hidden', true);
 }
 
 function getXScale(data) {
@@ -80,9 +80,10 @@ function renderYAxis(yScale) {
 
 function renderColumns(data) {
     const binWidth = (plotWidth - (plotPadding * 2)) / data.length;
+    const xScale = getXScale(data);
+    const yScale = getYScale(data);
+    const isReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 
-    const xScale = getXScale(data)
-    const yScale = getYScale(data)
 
     d3.select('#plot')
         .selectAll('rect')
@@ -93,7 +94,6 @@ function renderColumns(data) {
         .attr('data-date', ([date]) => date)
         .attr('data-gdp', ([_, value]) => value)
         .attr('width', binWidth)
-        .attr('height', () => yScale(0))
         .attr('x', ([date]) => xScale(new Date(date)))
         .attr('y', () => plotHeight - plotPadding - yScale(0))
         .on('mouseover', handleMouseOver)
@@ -104,13 +104,13 @@ function renderColumns(data) {
         .selectAll("rect")
         .transition()
         .ease(d3.easeCubicOut)
-        .duration(1000)
+        .duration(isReducedMotion ? 0 : 1000)
         .attr('y', ([_, value]) => plotHeight - plotPadding - yScale(value))
         .attr('height', ([_, value]) => yScale(value))
-        .delay((_, index) => index * 5);
+        .delay((_, index) => isReducedMotion ? 0 : index * 5);
 
-    renderXAxis(xScale)
-    renderYAxis(yScale)
+    renderXAxis(xScale);
+    renderYAxis(yScale);
 }
 
 setPlotSize();
